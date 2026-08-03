7549bd9bb4fcf7f236ea2425d419eeb8d5dc8f00
Lan Ly <lanly-dev@users.noreply.github.com>
2026-08-03 08:54:08 -0700

    chore: clean up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,45 @@
-import typescriptEslint from "typescript-eslint";
+import typescriptEslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [{
-    files: ["**/*.ts"],
+  ignores: ['node_modules']
 }, {
-    plugins: {
-        "@typescript-eslint": typescriptEslint.plugin,
-    },
-
-    languageOptions: {
-        parser: typescriptEslint.parser,
-        ecmaVersion: 2022,
-        sourceType: "module",
-    },
-
-    rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
-    },
-}];
+  files: ['**/*.ts', '**/*.mjs']
+}, {
+  plugins: {
+    '@typescript-eslint': typescriptEslint.plugin,
+    '@stylistic': stylistic
+  },
+  languageOptions: {
+    parser: typescriptEslint.parser,
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  rules: {
+    '@typescript-eslint/naming-convention': ['warn', { selector: 'import', format: ['camelCase', 'PascalCase'] }],
+    '@stylistic/member-delimiter-style': [
+      'error',
+      {
+        multiline: {
+          delimiter: 'none',
+          requireLast: false
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false
+        }
+      }
+    ],
+    'comma-dangle': ['error', 'never'],
+    'eol-last': ['error', 'always'],
+    'max-len': ['error', { code: 120 }],
+    'no-throw-literal': 'warn',
+    'no-trailing-spaces': 'error',
+    'quote-props': ['error', 'as-needed'],
+    curly: ['error', 'multi-or-nest'],
+    eqeqeq: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+    semi: ['error', 'never']
+  }
+}]
